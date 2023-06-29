63c2f60d9e2ef99f90404640f8c482767aa2b3e9
Jessica-ovabor <ovaborjessica85@gmail.com>
2023-06-29 23:35:37 +0100

    add carousel

--- runserver.py
+++ runserver.py
@@ -1,33 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from newsapi import NewsApiClient
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     newsapi = NewsApiClient(api_key="f4c5d7534dea4c5b854bd30e6688339f")
-    topheadlines = newsapi.get_top_headlines(
-    sources='al-jazeera-english'
-   )
-    articles =topheadlines['articles']
-    desc =[]
-    news=[]
-    img=[]
-    content=[]  
-    for i in range(len(articles)):
-        myarticles=articles[i]
+    topheadlines = newsapi.get_top_headlines(sources='al-jazeera-english')
+    articles = topheadlines['articles']
+    news = []
+    desc = []
+    img = []
+    content = []
+  
+    for article in articles:
+        news.append(article['title'])
+        desc.append(article['description'])
+        img.append(article['urlToImage'])
+        content.append(article['content'])
         
-        news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
-        content.append(myarticles['content'])
+    mylist = zip(news, desc, img, content)
         
-        
-    mylist =zip(news, desc,img,content)
-        
-    return render_template('index.html' , context=mylist)
-    
-if __name__ =="__main__":
+    return render_template('index.html', context=mylist)
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
-        
